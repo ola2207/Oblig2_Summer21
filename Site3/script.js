1bf6a9e32426fb72d6decb629769db6a2610f6e1
TheodorJaarvik <s365337@oslomet.no>
2021-10-13 16:50:55 +0200

    boingboing

---
+++ Site3/script.js
@@ -0,0 +1,61 @@
+var Bildernr;
+
+function nav_Onclick() {
+    
+    document.getElementById("div_bilde").innerHTML="";
+    
+    var img;
+    img = document.createElement('img');
+    img.src='../pictures/IMG_20210724_001116.jpg';
+    document.getElementById("div_bilde").appendChild(img);
+}
+
+function nav_Onclick2() {
+    
+    document.getElementById("div_bilde").innerHTML="";
+    
+    var img;
+    img = document.createElement('img');
+    img.src='../pictures/IMG_20210724_000644.jpg';
+    document.getElementById("div_bilde").appendChild(img);
+}
+
+function nav_Onclick3() {
+    
+    document.getElementById("div_bilde").innerHTML="";
+    
+    var img;
+    img = document.createElement('img');
+    img.src='../pictures/IMG_0143.jpg';
+    document.getElementById("div_bilde").appendChild(img);
+}
+
+function nav_Onclick4() {
+    
+    document.getElementById("div_bilde").innerHTML="";
+    
+    var img;
+    img = document.createElement('img');
+    img.src='../pictures/IMG_0195.jpeg';
+    document.getElementById("div_bilde").appendChild(img);
+}
+
+function nav_Onclick5() {
+    
+    document.getElementById("div_bilde").innerHTML="";
+    
+    var img;
+    img = document.createElement('img');
+    img.src='../pictures/IMG_0399.jpeg';
+    document.getElementById("div_bilde").appendChild(img);
+}
+
+function nav_Onclick6() {
+    
+    document.getElementById("div_bilde").innerHTML="";
+    
+    var img;
+    img = document.createElement('img');
+    img.src='../pictures/krabbesholm-gucci.jpeg';
+    document.getElementById("div_bilde").appendChild(img);
+}
